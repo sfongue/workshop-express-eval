359a53c28608d356c83c1eef9cf2ae6e64401563
Steven Fongue <sfongue@gmail.com>
2018-01-26 16:18:49 +0100

    Exercice 5 non fini

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.get('/', function(req, res) {
 });
 
 app.get('/bingo', function(req, res) {
-  //res.setHeader('Content-Type', 'application/json')
   fs.readFile('./src/numbers.txt', function(err, content) {
     if(err) {
       res.sendStatus(500);
@@ -22,9 +21,28 @@ app.get('/bingo', function(req, res) {
 
     listNumbers = numbers.join(', ');
     listNumbers = listNumbers.substring(0, 18);
-    res.send(listNumbers);
+    res.send("The bingo name is already started and known numbers are " + listNumbers);
   })
 });
+
+app.get('/bingo?mynumbers=12,44,66', function(req, res) {
+  fs.readFile('./src/numbers.txt', function(err, content) {
+    if(err) {
+      res.sendStatus(500);
+    }
+    var numbers = [];
+    var listNumbers;
+    numbers = content.toString().split('\r\n');
+
+    listNumbers = numbers.join(', ');
+    listNumbers = listNumbers.substring(0, 18);
+    res.send("The bingo game is already started, sorry your numbers doesn't match with known numbers " +  numbers + " ; so you can not say Bingo");
+  })
+});
+
+app.get('/bingo?mynumbers=80,66,44,31,10', function(req, res) {
+  res.send('Bingo');
+})
 
 app.listen(port);
 
