e4296658d92b18327dc1724a1532768197413fce
Steven Fongue <sfongue@gmail.com>
2018-01-26 15:02:46 +0100

    Exercice 1

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ const app = require('./src/app');
 // port
 const port = 3000;
 
+app.get('/', function(req, res) {
+  res.end('Hello World!')
+});
+
+app.listen(port);
+
 // start server here
 console.log(chalk.green('Hello web server'));
