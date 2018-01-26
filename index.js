95dd01e806c4daffc3b502cccad092b925d5782b
Steven Fongue <sfongue@gmail.com>
2018-01-26 16:08:54 +0100

    Exercice 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let chalk = require('chalk');
 
 const app = require('./src/app');
+const fs = require('fs');
 
 // port
 const port = process.argv[2];
@@ -10,7 +11,19 @@ app.get('/', function(req, res) {
 });
 
 app.get('/bingo', function(req, res) {
-  res.end('Bingo')
+  //res.setHeader('Content-Type', 'application/json')
+  fs.readFile('./src/numbers.txt', function(err, content) {
+    if(err) {
+      res.sendStatus(500);
+    }
+    var numbers = [];
+    var listNumbers;
+    numbers = content.toString().split('\r\n');
+
+    listNumbers = numbers.join(', ');
+    listNumbers = listNumbers.substring(0, 18);
+    res.send(listNumbers);
+  })
 });
 
 app.listen(port);
